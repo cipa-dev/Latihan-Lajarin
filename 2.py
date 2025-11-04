725b14ed4470f8583aff4b45e2009c69f705bed6
unknown <kerensensiadestiana@gmail.com>
2025-11-04 15:06:41 +0800

    sepuh

--- 2.py
+++ 2.py
@@ -1,1 +1,17 @@
-print("Biliie Eilish")
+# print("Biliie Eilish")
+
+nama="esti"
+value= "nama sembarangan"
+# string 
+nama="17"
+value= "umur sembarangan"
+#integer
+nama="turu"
+value= "hobi sembarangan"
+#string
+nama = "apakah senang" 
+value= True/False
+#boolean
+nama= "phi"
+value=3,14
+#float
