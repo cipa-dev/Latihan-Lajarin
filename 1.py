739eab51c00bfbff5607cadfd9c06163cc03b1b4
cipa-dev <risal9719@gmail.com>
2025-11-04 15:49:26 +0800

    latihan2

--- 1.py
+++ 1.py
@@ -1,14 +1,20 @@
 # print ("lapar")
 # tipe string
-nama = "cipa" 
-umur = "18 tahun"
-hobi = "renang"
-# tipe boolean
-apakahsenang = True
-# tipe integer
-phi = 3.14
+# nama = "cipa" 
+# umur = "18 tahun"
+# hobi = "renang"
+# # tipe boolean
+# apakahsenang = True
+# # tipe integer
+# phi = 3.14
 
 # ini betul
+
+umur = 18
+if umur >=17:
+    print("kamu dewasa!")
+else:
+    print("kamu anak anak")
 
 
 
